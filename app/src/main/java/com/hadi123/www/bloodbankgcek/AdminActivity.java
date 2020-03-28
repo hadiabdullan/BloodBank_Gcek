0bc2db483ccef5008a0790d0bd90dbd87f9f1829
Hadi Abdulla N <mhadi97@gmail.com>
2020-03-28 14:05:46 +0530

    Added Search optimization with blood group

--- app/src/main/java/com/hadi123/www/bloodbankgcek/AdminActivity.java
+++ app/src/main/java/com/hadi123/www/bloodbankgcek/AdminActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -34,12 +37,36 @@ public class AdminActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setHasFixedSize(true);
         
-        LoadData();
+        LoadData("");
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString()!= null){
+                    LoadData(s.toString());
+                }
+                else{
+                    LoadData("");
+                }
+            }
+        });
 
     }
 
-    private void LoadData() {
-        options=new FirebaseRecyclerOptions.Builder<Users>().setQuery(DataRef,Users.class).build();
+    private void LoadData(String data) {
+        Query query = DataRef.orderByChild("Bloodgroup").startAt(data).endAt(data + "\uf8ff");
+
+        options=new FirebaseRecyclerOptions.Builder<Users>().setQuery(query,Users.class).build();
         adapter= new FirebaseRecyclerAdapter<Users, MyViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Users model) {
